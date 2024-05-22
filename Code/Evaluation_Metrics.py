0cbe66590a9f410d90fb6924adaedf92f00d6e43
Angelido <a.nardone5@studenti.unipi.it>
2024-05-22 13:12:07 +0200

    Add functions

--- Code/Evaluation_Metrics.py
+++ Code/Evaluation_Metrics.py
@@ -43,5 +43,48 @@ def report_scores(test_label, test_pred, labels=None):
     """
     # Print the classification report using sklearn's classification_report function
     print(classification_report(test_label, test_pred, labels=labels))
+    
+def plot_accuracy(train_accuracies, val_accuracies=None, figsize=(8,6), print_every=1):
+    """
+    Plot training and validation accuracies.
+
+    Args:
+        train_accuracies (list): List of training accuracies.
+        figsize (tuple): Size of the figure.
+        val_accuracies (list): List of validation accuracies (optional).
+        print_every (int): Interval for printing training progress.
+    """
+    epochs = range(1, len(train_accuracies) + 1)
+    plt.figure(figsize=figsize)
+    plt.plot(epochs, train_accuracies, label='Train Accuracy')
+    if val_accuracies:
+        plt.plot(epochs, val_accuracies, label='Val Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training and Validation Accuracies')
+    plt.legend()
+    plt.xticks(epochs[::print_every])
+    plt.show()
 
     
+def plot_loss(train_losses, val_losses=None, figsize=(8,6), print_every=1):
+    """
+    Plot training and validation losses.
+
+    Args:
+        train_losses (list): List of training losses.
+        val_losses (list): List of validation losses (optional).
+        figsize (tuple): Size of the figure.
+        print_every (int): Interval for printing training progress.
+    """
+    epochs = range(1, len(train_losses) + 1)
+    plt.figure(figsize=figsize)
+    plt.plot(epochs, train_losses, label='Train Loss')
+    if val_losses:
+        plt.plot(epochs, val_losses, label='Val Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Losses')
+    plt.legend()
+    plt.xticks(epochs[::print_every])
+    plt.show()
